195b7fecf9f62424d133a5da10434c2fd1ff4e4f
moisesdj@uninorte.edu.co <moisesdj@uninorte.edu.co>
2026-05-23 21:24:44 -0500

    solid database

--- src/packagee/core/hospital/DataBase.java
+++ src/packagee/core/hospital/DataBase.java
@@ -24,43 +24,111 @@ import packagee.Specialty;
  * @author juand
  */
 
+public interface UserRepository {
 
-public class DataBase {
+    void addUser(User user);
 
-    private static DataBase instance;
+    Optional<User> findUser(String username);
 
-    public static DataBase getInstance() {
-        if (instance == null) {
-            instance = new DataBase();
-        }
-        return instance;
+    Optional<User> findID(long id);
+
+    ArrayList<User> getUsers();
+
+    ArrayList<Doctor> getDoctors();
+}
+
+public interface AppointmentRepository {
+
+    void addAppointment(Appointment appointment);
+
+    Optional<Appointment> findIDapp(String id);
+
+    ArrayList<Appointment> getAppointments();
+}
+
+
+public interface HospitalizationRepository {
+
+    void addHospitalization(Hospitalization hospitalization);
+
+    Optional<Hospitalization> findHospitalizationID(String id);
+
+    ArrayList<Hospitalization> getHospitalizations();
+}
+
+public interface AppointmentIDGenerator {
+    String generateAppID(long patientId);
+}
+
+public interface HospitalizationIDGenerator {
+    String genHospitalizationID(long patientId);
+}
+
+public class AppointmentCounterGenerator implements AppointmentIDGenerator {
+
+    private final HashMap<String, Integer> appointmentCounters =
+            new HashMap<>();
+
+    @Override
+    public String generateAppID(long patientId) {
+
+        String key = String.valueOf(patientId);
+
+        int counter = appointmentCounters.getOrDefault(key, 0);
+
+        String id = String.format("A-%s-%04d", key, counter);
+
+        appointmentCounters.put(key, counter + 1);
+
+        return id;
     }
+}
 
-    private ArrayList<User> users;
-    private ArrayList<Appointment> appointments;
-    private ArrayList<Hospitalization> hospitalizations;
+public class HospitalizationCounterGenerator
+        implements HospitalizationIDGenerator {
 
-    private java.util.HashMap<String, Integer> appointmentCounters;
-    private java.util.HashMap<String, Integer> hospitalizationCounters;
+    private final HashMap<String, Integer> hospitalizationCounters =
+            new HashMap<>();
 
-    private DataBase() {
-        this.users = new ArrayList<>();
-        this.appointments = new ArrayList<>();
-        this.hospitalizations = new ArrayList<>();
-        this.appointmentCounters = new java.util.HashMap<>();
-        this.hospitalizationCounters = new java.util.HashMap<>();
+    @Override
+    public String genHospitalizationID(long patientId) {
 
-        cargarJSON("C:\\Users\\juand\\OneDrive\\Desktop\\Ospedale\\json\\users.json");
+        String key = String.valueOf(patientId);
+
+        int counter = hospitalizationCounters.getOrDefault(key, 0);
+
+        String id = String.format("H-%s-%04d", key, counter);
+
+        hospitalizationCounters.put(key, counter + 1);
+
+        return id;
     }
+}
 
-    private void cargarJSON(String path) {
+public interface UserLoader {
+    ArrayList<User> cargarJSON(String path);
+}
+
+public class JSONUserLoader implements UserLoader {
+
+    @Override
+    public ArrayList<User> cargarJSON(String path) {
+
+        ArrayList<User> users = new ArrayList<>();
+
         try {
-            String content = new String(Files.readAllBytes(Paths.get(path)));
+
+            String content =
+                    new String(Files.readAllBytes(Paths.get(path)));
+
             JSONObject root = new JSONObject(content);
+
             JSONArray usersArray = root.getJSONArray("users");
 
             for (int i = 0; i < usersArray.length(); i++) {
+
                 JSONObject obj = usersArray.getJSONObject(i);
+
                 String type = obj.getString("type");
 
                 long id = obj.getLong("id");
@@ -70,164 +138,386 @@ public class DataBase {
                 String password = obj.getString("password");
 
                 switch (type) {
+
                     case "admin":
-                        users.add(new Administrator(id, username, firstname, lastname, password));
+
+                        users.add(
+                                new Administrator(
+                                        id,
+                                        username,
+                                        firstname,
+                                        lastname,
+                                        password
+                                )
+                        );
+
                         break;
+
                     case "patient":
+
                         String email = obj.getString("email");
-                        LocalDate birthdate = LocalDate.parse(obj.getString("birthdate"));
-                        boolean gender = obj.getBoolean("gender");
-                        long phone = obj.getLong("phone");
-                        String address = obj.getString("address");
-                        users.add(new Patient(id, username, firstname, lastname, password,
-                                email, birthdate, gender, phone, address));
+
+                        LocalDate birthdate =
+                                LocalDate.parse(
+                                        obj.getString("birthdate")
+                                );
+
+                        boolean gender =
+                                obj.getBoolean("gender");
+
+                        long phone =
+                                obj.getLong("phone");
+
+                        String address =
+                                obj.getString("address");
+
+                        users.add(
+                                new Patient(
+                                        id,
+                                        username,
+                                        firstname,
+                                        lastname,
+                                        password,
+                                        email,
+                                        birthdate,
+                                        gender,
+                                        phone,
+                                        address
+                                )
+                        );
+
                         break;
+
                     case "doctor":
-                        String specialtyStr = obj.getString("specialty");
-                        Specialty specialty = parseSpecialty(specialtyStr);
-                        String licenceNumber = obj.getString("licenceNumber");
-                        String assignedOffice = obj.getString("assignedOffice");
-                        users.add(new Doctor(id, username, firstname, lastname, password,
-                                specialty, licenceNumber, assignedOffice));
+
+                        String specialtyStr =
+                                obj.getString("specialty");
+
+                        Specialty specialty =
+                                parseSpecialty(specialtyStr);
+
+                        String licenceNumber =
+                                obj.getString("licenceNumber");
+
+                        String assignedOffice =
+                                obj.getString("assignedOffice");
+
+                        users.add(
+                                new Doctor(
+                                        id,
+                                        username,
+                                        firstname,
+                                        lastname,
+                                        password,
+                                        specialty,
+                                        licenceNumber,
+                                        assignedOffice
+                                )
+                        );
+
                         break;
                 }
             }
+
         } catch (IOException e) {
-            System.err.println("No se pudo cargar users.json: " + e.getMessage());
+
+            throw new RuntimeException(
+                    "No se pudo cargar users.json",
+                    e
+            );
         }
+
+        return users;
     }
 
     private Specialty parseSpecialty(String specialtyStr) {
+
         switch (specialtyStr.toUpperCase()) {
+
             case "ORTHOPEDICS":
             case "TRAUMATOLOGY_ORTHOPEDICS":
+
                 return Specialty.TRAUMATOLOGY_ORTHOPEDICS;
+
             case "GYNECOLOGY":
             case "GYNECOLOGY_OBSTETRICS":
+
                 return Specialty.GYNECOLOGY_OBSTETRICS;
+
             default:
-                return Specialty.valueOf(specialtyStr.toUpperCase());
+
+                return Specialty.valueOf(
+                        specialtyStr.toUpperCase()
+                );
         }
     }
+}
 
+public class InMemoryUserRepository
+        implements UserRepository {
+
+    private final ArrayList<User> users;
+
+    public InMemoryUserRepository(
+            UserLoader loader,
+            String path
+    ) {
+
+        this.users = loader.cargarJSON(path);
+    }
+
+    @Override
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public Optional<User> findUser(String username) {
+
+        return users.stream()
+                .filter(u ->
+                        u.getUsername().equals(username)
+                )
+                .findFirst();
+    }
+
+    @Override
+    public Optional<User> findID(long id) {
+
+        return users.stream()
+                .filter(u ->
+                        u.getId() == id
+                )
+                .findFirst();
+    }
+
+    @Override
     public ArrayList<User> getUsers() {
         return users;
     }
 
-    public void addUser(User user) {
-        this.users.add(user);
-    }
-
-    public User findUser(String username) {
-        for (User u : users) {
-            if (u.getUsername().equals(username)) {
-                return u;
-            }
-        }
-        return null;
-    }
-
-    public User findID(long id) {
-        for (User u : users) {
-            if (u.getId() == id) {
-                return u;
-            }
-        }
-        return null;
-    }
-
-    public boolean userExists(String username) {
-        return findUser(username) != null;
-    }
-
-    public boolean idExists(long id) {
-        return findID(id) != null;
-    }
-
-    public ArrayList<Appointment> getAppointments() {
-        return appointments;
-    }
-
-    public void addAppointment(Appointment appointment) {
-        this.appointments.add(appointment);
-    }
-
-    public Appointment findIDapp(String id) {
-        for (Appointment a : appointments) {
-            if (a.getId().equals(id)) {
-                return a;
-            }
-        }
-        return null;
-    }
-
-    public String generateAppID(long patientId) {
-        String key = String.valueOf(patientId);
-        int counter = appointmentCounters.getOrDefault(key, 0);
-        String id = String.format("A-%s-%04d", key, counter);
-        appointmentCounters.put(key, counter + 1);
-        return id;
-    }
-
-    public ArrayList<Hospitalization> getHospitalizations() {
-        return hospitalizations;
-    }
-
-    public void addHospitalization(Hospitalization hospitalization) {
-        this.hospitalizations.add(hospitalization);
-    }
-
-    public Hospitalization findHospitalizationID(String id) {
-        for (Hospitalization h : hospitalizations) {
-            if (h.getId().equals(id)) {
-                return h;
-            }
-        }
-        return null;
-    }
-
-    
-    public String genHospitalizationID(long patientId) {
-        String key = String.valueOf(patientId);
-        int counter = hospitalizationCounters.getOrDefault(key, 0);
-        String id = String.format("H-%s-%04d", key, counter);
-        hospitalizationCounters.put(key, counter + 1);
-        return id;
-    }
-
+    @Override
     public ArrayList<Doctor> getDoctors() {
-        ArrayList<Doctor> doctors = new ArrayList<>();
+
+        ArrayList<Doctor> doctors =
+                new ArrayList<>();
+
         for (User u : users) {
+
             if (u instanceof Doctor) {
                 doctors.add((Doctor) u);
             }
         }
+
         return doctors;
     }
+}
+public class InMemoryAppointmentRepository
+        implements AppointmentRepository {
 
-    public ArrayList<Doctor> getdoctorsSpeciality(Specialty specialty, java.time.LocalDateTime datetime) {
-        ArrayList<Doctor> available = new ArrayList<>();
-        for (Doctor d : getDoctors()) {
-            if (d.getSpecialty() == specialty && doctorAvailable(d, datetime)) {
-                available.add(d);
-            }
-        }
-        return available;
+    private final ArrayList<Appointment> appointments =
+            new ArrayList<>();
+
+    @Override
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
     }
 
-    public boolean doctorAvailable(Doctor doctor, java.time.LocalDateTime datetime) {
-        java.time.LocalDateTime end = datetime.plusMinutes(15);
-        for (Appointment a : appointments) {
+    @Override
+    public Optional<Appointment> findIDapp(String id) {
+
+        return appointments.stream()
+                .filter(a ->
+                        a.getId().equals(id)
+                )
+                .findFirst();
+    }
+
+    @Override
+    public ArrayList<Appointment> getAppointments() {
+        return appointments;
+    }
+}
+
+public class InMemoryHospitalizationRepository
+        implements HospitalizationRepository {
+
+    private final ArrayList<Hospitalization> hospitalizations =
+            new ArrayList<>();
+
+    @Override
+    public void addHospitalization(
+            Hospitalization hospitalization
+    ) {
+
+        hospitalizations.add(hospitalization);
+    }
+
+    @Override
+    public Optional<Hospitalization> findHospitalizationID(
+            String id
+    ) {
+
+        return hospitalizations.stream()
+                .filter(h ->
+                        h.getId().equals(id)
+                )
+                .findFirst();
+    }
+
+    @Override
+    public ArrayList<Hospitalization> getHospitalizations() {
+        return hospitalizations;
+    }
+}
+
+public class DoctorAvailabilityService {
+
+    private final AppointmentRepository appointmentRepository;
+
+    public DoctorAvailabilityService(
+            AppointmentRepository appointmentRepository
+    ) {
+
+        this.appointmentRepository =
+                appointmentRepository;
+    }
+
+    public boolean doctorAvailable(
+            Doctor doctor,
+            LocalDateTime datetime
+    ) {
+
+        LocalDateTime end =
+                datetime.plusMinutes(15);
+
+        for (Appointment a :
+                appointmentRepository.getAppointments()) {
+
             if (a.getDoctor().getId() == doctor.getId()
-                    && a.getStatus() != AppointmentStatus.CANCELED
-                    && a.getStatus() != AppointmentStatus.COMPLETED) {
-                java.time.LocalDateTime aStart = a.getDatetime();
-                java.time.LocalDateTime aEnd = aStart.plusMinutes(15);
-                if (datetime.isBefore(aEnd) && end.isAfter(aStart)) {
+                    && a.getStatus()
+                    != AppointmentStatus.CANCELED
+                    && a.getStatus()
+                    != AppointmentStatus.COMPLETED) {
+
+                LocalDateTime aStart =
+                        a.getDatetime();
+
+                LocalDateTime aEnd =
+                        aStart.plusMinutes(15);
+
+                if (datetime.isBefore(aEnd)
+                        && end.isAfter(aStart)) {
+
                     return false;
                 }
             }
         }
+
         return true;
+    }
+
+    public ArrayList<Doctor> getdoctorsSpeciality(
+            Specialty specialty,
+            LocalDateTime datetime,
+            UserRepository userRepository
+    ) {
+
+        ArrayList<Doctor> available =
+                new ArrayList<>();
+
+        for (Doctor d :
+                userRepository.getDoctors()) {
+
+            if (d.getSpecialty() == specialty
+                    && doctorAvailable(d, datetime)) {
+
+                available.add(d);
+            }
+        }
+
+        return available;
+    }
+}
+
+
+public class ApplicationContext {
+
+    private final UserRepository userRepository;
+
+    private final AppointmentRepository
+            appointmentRepository;
+
+    private final HospitalizationRepository
+            hospitalizationRepository;
+
+    private final AppointmentIDGenerator
+            appointmentIDGenerator;
+
+    private final HospitalizationIDGenerator
+            hospitalizationIDGenerator;
+
+    private final DoctorAvailabilityService
+            doctorAvailabilityService;
+
+    public ApplicationContext() {
+
+        UserLoader loader =
+                new JSONUserLoader();
+
+        this.userRepository =
+                new InMemoryUserRepository(
+                        loader,
+                        "C:\\Users\\juand\\OneDrive\\Desktop\\Ospedale\\json\\users.json"
+                );
+
+        this.appointmentRepository =
+                new InMemoryAppointmentRepository();
+
+        this.hospitalizationRepository =
+                new InMemoryHospitalizationRepository();
+
+        this.appointmentIDGenerator =
+                new AppointmentCounterGenerator();
+
+        this.hospitalizationIDGenerator =
+                new HospitalizationCounterGenerator();
+
+        this.doctorAvailabilityService =
+                new DoctorAvailabilityService(
+                        appointmentRepository
+                );
+    }
+
+    public UserRepository getUserRepository() {
+        return userRepository;
+    }
+
+    public AppointmentRepository getAppointmentRepository() {
+        return appointmentRepository;
+    }
+
+    public HospitalizationRepository
+    getHospitalizationRepository() {
+
+        return hospitalizationRepository;
+    }
+
+    public AppointmentIDGenerator
+    getAppointmentIDGenerator() {
+
+        return appointmentIDGenerator;
+    }
+
+    public HospitalizationIDGenerator
+    getHospitalizationIDGenerator() {
+
+        return hospitalizationIDGenerator;
+    }
+
+    public DoctorAvailabilityService
+    getDoctorAvailabilityService() {
+
+        return doctorAvailabilityService;
     }
 }
